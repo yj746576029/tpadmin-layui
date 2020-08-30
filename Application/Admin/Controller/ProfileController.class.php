dbe779768d726fd9cfaadfd4f451730f34f69b65
yangjie <746576029@qq.com>
2020-08-30 20:07:07 +0800

    修改账号密码

--- Application/Admin/Controller/ProfileController.class.php
+++ Application/Admin/Controller/ProfileController.class.php
@@ -9,6 +9,8 @@ class ProfileController extends BaseController
     public function index()
     {
         $user = session('user');
+        $user = M('User')->where(['id' => $user['id']])->find();
+        unset($user['password'], $user['salt']);
         $this->assign('user', $user);
         $this->display();
     }
@@ -16,26 +18,62 @@ class ProfileController extends BaseController
     public function edit()
     {
         if (IS_POST) {
-            $userName = I('post.user_name');
+            $id = I('post.id');
+            $data['realname'] = I('post.realname');
+            $data['mobile'] = I('post.mobile');
+            $data['email'] = I('post.email');
+            $re = M('User')->where(['id' => $id])->save($data);
+            if ($re) {
+                $data['code']=0;
+                $data['msg']='修改成功';
+                $this->ajaxReturn($data);
+            } else {
+                $data['code']=1;
+                $data['msg']='修改失败';
+                $this->ajaxReturn($data);
+            }
+        }
+    }
+
+    public function password()
+    {
+        $user = session('user');
+        $this->assign('user', $user);
+        $this->display();
+    }
+
+    public function passwordEdit()
+    {
+        if (IS_POST) {
+            $id = I('post.id');
             $password = I('post.password');
-            $password2 = I('post.password2');
-            $user = M('User')->where(['user_name' => $userName])->find();
+            $passwordNew = I('post.passwordNew');
+            $repasswordNew = I('post.repasswordNew');
+            if($passwordNew!==$repasswordNew){
+                $data['code']=1;
+                $data['msg']='两次密码不一致';
+                $this->ajaxReturn($data);
+            }
+            $user = M('User')->where(['id' => $id])->find();
             if ($user['password'] === md5(md5($password) . $user['salt'])) {
-                $data['password'] = md5(md5($password2) . $user['salt']);
-                $re = M('User')->where(['user_name' => $user['user_name']])->save($data);
+                $data['password'] = md5(md5($passwordNew) . $user['salt']);
+                $re = M('User')->where(['id' => $id])->save($data);
                 if ($re) {
                     session('user', null);
-                    $this->success('修改成功，请重新登录', U('admin/login/index'));
+                    $data['code']=0;
+                    $data['msg']='修改成功，请重新登录';
+                    $this->ajaxReturn($data);
                 } else {
-                    $this->error('修改失败');
+                    $data['code']=1;
+                    $data['msg']='修改失败';
+                    $this->ajaxReturn($data);
                 }
             } else {
+                $data['code']=1;
+                $data['msg']='原始密码错误';
+                $this->ajaxReturn($data);
                 $this->error('原始密码错误');
             }
-        } else {
-            $user = session('user');
-            $this->assign('user', $user);
-            $this->display();
         }
     }
 }
